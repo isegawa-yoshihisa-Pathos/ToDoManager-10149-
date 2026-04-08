a578451acd4f1f76aab1d3fbe72a783633879014
Yoshihisa Isegawa <yoshihisa.isegawa@pathoslogos.co.jp>
2026-04-08 15:48:54 +0900

    2026/04/08/15:48/changed

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,14 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TaskList } from './task-list/task-list';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, FormsModule, TaskList],
+  imports: [CommonModule, FormsModule, TaskList, NzPageHeaderModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
-  
 }
